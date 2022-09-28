88b4de107788259872a082a4535493e3739512da
Mykhailo Gryshchenko <mykhailogryshchenko@cloud.upwork.com>
2022-09-28 21:05:52 +0300

    fix element generator script

--- generate-element-exports.js
+++ generate-element-exports.js
@@ -2,11 +2,11 @@ import fs from 'fs';
 import tags from './html-tags.json' assert { type: "json" };
 const SKIP_TAGS = ['var'];
 let content = `
-import { h } from './html';
+import { h } from 'guigna';
 const c = (query) => (...args) => h(query, ...args);`;
 for(let tag of tags) {
   if (SKIP_TAGS.includes(tag)) continue;
   content += `
 export const ${tag} = c('${tag}');`;
 }
-fs.writeFileSync('./src/elements.ts', content);
+fs.writeFileSync('./elements/elements.ts', content);
